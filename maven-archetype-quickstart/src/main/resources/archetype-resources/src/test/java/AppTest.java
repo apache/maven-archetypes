7b98de81a60f98467889680f4224ea50ccc690c7
Karl Heinz Marbaise <khmarbaise@apache.org>
2014-06-02 19:55:54 +0000

    [MARCHETYPES-45]  - maven-archetype-profiles  - maven-archetype-quickstart  - maven-archetype-webapp    - Upgrade to JUnit 4.11    - usage of annotations for unit test example.
    git-svn-id: https://svn.apache.org/repos/asf/maven/archetypes/trunk@1599333 13f79535-47bb-0310-9956-ffa450edef68

--- maven-archetype-quickstart/src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ maven-archetype-quickstart/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -1,37 +1,19 @@
 package $package;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    @Test
+    public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
