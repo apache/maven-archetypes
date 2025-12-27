5d66f21c20b4475ba4d250cad3f5f0dca94e14a0
Slawomir Jaranowski <s.jaranowski@gmail.com>
2025-12-27 20:39:06 +0100

    Remove JUnit 3 references, drop usage of TestCase

--- maven-archetype-plugin/src/main/resources/archetype-resources/src/test/java/MyMojoTest.java
+++ maven-archetype-plugin/src/main/resources/archetype-resources/src/test/java/MyMojoTest.java
@@ -16,64 +16,48 @@ package $package;
 ## limitations under the License.
 ##
 
-import org.apache.maven.plugin.testing.MojoRule;
-import org.apache.maven.plugin.testing.WithoutMojo;
+import org.apache.maven.api.plugin.testing.InjectMojo;
+import org.apache.maven.api.plugin.testing.MojoTest;
+import org.apache.maven.api.plugin.testing.MojoExtension;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Rule;
-import static org.junit.Assert.*;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.apache.maven.api.plugin.testing.MojoExtension.getBasedir;
+
 import java.io.File;
 
-public class MyMojoTest
-{
-    @Rule
-    public MojoRule rule = new MojoRule()
-    {
-        @Override
-        protected void before() throws Throwable 
-        {
-        }
-
-        @Override
-        protected void after()
-        {
-        }
-    };
+@MojoTest
+public class MyMojoTest {
 
     /**
      * @throws Exception if any
      */
     @Test
-    public void testSomething()
-            throws Exception
-    {
-        File pom = new File( "target/test-classes/project-to-test/" );
-        assertNotNull( pom );
-        assertTrue( pom.exists() );
+    @InjectMojo(goal = "touch", pom = "classpath:/project-to-test/pom.xml")
+    public void testSomething(MyMojo myMojo) throws Exception {
 
-        MyMojo myMojo = ( MyMojo ) rule.lookupConfiguredMojo( pom, "touch" );
-        assertNotNull( myMojo );
         myMojo.execute();
 
-        File outputDirectory = ( File ) rule.getVariableValueFromObject( myMojo, "outputDirectory" );
-        assertNotNull( outputDirectory );
-        assertTrue( outputDirectory.exists() );
+        File outputDirectory = (File)MojoExtension.getVariableValueFromObject(myMojo, "outputDirectory");
+        assertNotNull(outputDirectory);
+        assertTrue(outputDirectory.exists());
 
-        File touch = new File( outputDirectory, "touch.txt" );
-        assertTrue( touch.exists() );
+        File touch = new File(outputDirectory, "touch.txt");
+        assertTrue(touch.exists());
 
-        File expectedOutputDirectory = new File
-          (pom.getAbsoluteFile(), "target/test-harness/project-to-test");
+        File expectedOutputDirectory = new File(getBasedir(), "target/test-harness/project-to-test");
         assertEquals(expectedOutputDirectory, outputDirectory);
     }
 
-    /** Do not need the MojoRule. */
-    @WithoutMojo
+    /**
+     * Do not need the Mojo.
+     */
     @Test
-    public void testSomethingWhichDoesNotNeedTheMojoAndProbablyShouldBeExtractedIntoANewClassOfItsOwn()
-    {
-        assertTrue( true );
+    public void testSomethingWhichDoesNotNeedTheMojoAndProbablyShouldBeExtractedIntoANewClassOfItsOwn() {
+        assertTrue(true);
     }
-
 }
 
