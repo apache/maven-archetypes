14b2159066a2952aa18e29984e18ab281e9e3a99
Slawomir Jaranowski <s.jaranowski@gmail.com>
2025-12-27 20:39:06 +0100

    Remove JUnit 3 references, drop usage of TestCase

--- maven-archetype-simple/src/main/resources/archetype-resources/src/main/java/App.java
+++ maven-archetype-simple/src/main/resources/archetype-resources/src/main/java/App.java
@@ -4,10 +4,8 @@ package ${package};
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
